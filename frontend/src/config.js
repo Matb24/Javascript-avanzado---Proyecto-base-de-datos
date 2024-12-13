c1ae96c63ce94f3067d707f784c3007fd384cd99
Matb24 <93958691+Matb24@users.noreply.github.com>
2024-12-13 00:33:52 -0500

    Update config.js
    cambio de host

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
-export const SECRET="secretito";
+export const SECRET = "secretito";
 
-export const API_URL_AUTH = "http://localhost:5001/api/auth";
-export const API_URL_ALMACEN = "http://localhost:5001/api/Almacen";
+export const API_URL_AUTH = "https://almacen-backend.onrender.com/api/auth";
+export const API_URL_ALMACEN = "https://almacen-backend.onrender.com/api/Almacen";
 
